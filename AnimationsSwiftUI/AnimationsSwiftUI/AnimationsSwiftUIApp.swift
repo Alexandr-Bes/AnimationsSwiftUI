80796925c5c71534107693784ac34bfaa85876fd
Alex Bezkopylnyi <>
2023-05-13 20:53:06 +0100

    basic project setup

---
+++ AnimationsSwiftUI/AnimationsSwiftUI/AnimationsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsSwiftUIApp.swift
+//  AnimationsSwiftUI
+//
+//  Created by AlexBezkopylnyi on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
